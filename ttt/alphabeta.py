2fc1b7824d72abc9d5560529fc6087cda86ab64b
Peter Stenger <peter.a.stenger@gmail.com>
2016-12-12 18:09:12 -0500

    save

--- ttt/alphabeta.py
+++ ttt/alphabeta.py
@@ -1,20 +1,30 @@
 #Using wikipedia pseudocode w/o depth
-def alpha_beta(game_state, alpha, beta):
-    if game_state.get_score() is not None:
-        return 
-
-def max_play(game_state, alpha, beta):
+def alphabeta(game_state, alpha, beta, our_turn=True):
     if game_state.is_gameover():
-        return game_state.score()
-
-def min_play(game_state, alpha, beta):
-
-    if game_state.is_gameover():
-        return game_state.score()
-    moves = game_state.get_possible_moves()
-    for move in moves:
-        copy = game_state.get_next_state(move, False)
-        score = max_play(copy)
-
-        if beta <= alpha:
-            break
+        return (None, game_state.score())
+    if our_turn:
+        score = -9999
+        for move in game_state.get_possible_moves():
+            child = game_state.get_next_state(move, True)
+            temp_max = alphabeta(child, alpha, beta, False)
+            if temp_max[1] > score:
+                print(score)
+                score = temp_max[1]
+                max_tuple = temp_max
+            alpha = max(alpha, score)
+            if beta <= alpha:
+                break
+        return max_tuple
+    else:
+        score = 9999
+        for move in game_state.get_possible_moves():
+            child = game_state.get_next_state(move, False)
+            temp_min = alphabeta(child, alpha, beta, True)
+            if temp_min[1] < score:
+                score = temp_min[1]
+                min_tuple = temp_min
+            beta = min(beta, score)
+            if beta <= alpha:
+                break
+        return min_tuple
+    
