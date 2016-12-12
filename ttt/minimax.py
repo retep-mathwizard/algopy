a4b6ba706391068532f119ccd3f64aabe2ea6c08
Peter Stenger <peter.a.stenger@gmail.com>
2016-12-12 11:42:18 -0500

    Update minimax.py

--- ttt/minimax.py
+++ ttt/minimax.py
@@ -1,18 +1,17 @@
 #http://giocc.com/concise-implementation-of-minimax-through-higher-order-functions.html
 #Review - http://codereview.stackexchange.com/questions/149363/minimax-tic-tac-toe-implementation/149605#149605
-#((move, 
-#play(game_state.get_next_state(move, our_turn)), not our_turn) for move in game_state.get_possible_moves()))
+from operator import itemgetter
 def minimax(game_state, our_turn):
-    '''returns the max of mapping the (move, score) tuple to the possible move using the second part of the tuple (score)'''
     score = game_state.get_score()
     if score is not None:
-        return None, score
-    #returns the move score pairs, using opposites (not our_turn
-    '''
-    for move in game_state.get_possible_moves:
-        moves.append((move, minimax(game_state.get_next_state(move, our_turn), not our_turn)))
-    '''
-    moves = ((move, minimax(game_state.get_next_state(move, our_turn)), not our_turn) for move in game_state.get_possible_moves())
-
+        return (None, score)
+    #returns the move score pairs, using opposites (not our_turn) 
+    #tuple of move, score for move 
+    for move in game_state.get_possible_moves():
+        moves = ((move, minimax(game_state.get_next_state(move, our_turn)), not our_turn)
+    if our_turn:
+        return max(moves, key=itemgetter(1))
+    else:
+        return min(moves, key=itemgetter(1))
     return (max if our_turn else min)(moves, key=itemgetter(1))
     
