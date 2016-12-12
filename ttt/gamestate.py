aa45e5df3b991977194bbae1775a80dff1de1751
Peter Stenger <peter.a.stenger@gmail.com>
2016-12-12 11:32:17 -0500

    Update gamestate.py

--- ttt/gamestate.py
+++ ttt/gamestate.py
@@ -5,15 +5,16 @@ class GameState:
         self.board = board
         self.winning_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-    def is_gameover(self):
+    def get_score(self):
         '''returns if a game_state has been won or filled up'''
         if self.board.count(self.char) + self.board.count(self.oppchar) == 9:
-            return True
+            return 0
         for combo in self.winning_combos:
-            if (self.board[combo[0]] == self.char and self.board[combo[1]] == self.char and self.board[combo[2]] == self.char) or \
-            (self.board[combo[0]] == self.oppchar and self.board[combo[1]] == self.oppchar and self.board[combo[2]] == self.oppchar):
-                return True
-        return False
+            if self.board[combo[0]] == self.char and self.board[combo[1]] == self.char and self.board[combo[2]] == self.char:
+                return 1    
+            elif self.board[combo[0]] == self.oppchar and self.board[combo[1]] == self.oppchar and self.board[combo[2]] == self.oppchar:
+                return -1         
+        return None
 
     @staticmethod
     def return_state(score):
@@ -34,27 +35,12 @@ class GameState:
 
     def get_possible_moves(self):
         '''returns all possible squares to place a character'''
-        squares = []
-        for index, square in enumerate(self.board):
-            if square != self.char and square != self.oppchar:
-                squares.append(index)
-        return squares
+        return [index for index, square in enumerate(self.board) if square != self.char and square != self.oppchar]:
 
     def get_next_state(self, move, our_turn):
         '''returns the gamestate with the move filled in'''
         copy = self.board[:]
-        if our_turn:
-            copy[move] = self.char
-        else:
-            copy[move] = self.oppchar
+        copy[move] = self.char if our_turn else self.oppchar
         return GameState(copy, char=self.char, oppchar=self.oppchar)
 
-    def score(self):
-        '''score a game_state from the computers point of view, 1 = win, 0 = tie, -1 = lose'''
-        for combo in self.winning_combos:
-            if self.board[combo[0]] == self.char and self.board[combo[1]] == self.char and self.board[combo[2]] == self.char:
-                return 1    
-            elif self.board[combo[0]] == self.oppchar and self.board[combo[1]] == self.oppchar and self.board[combo[2]] == self.oppchar:
-                return -1
-        return 0
 
