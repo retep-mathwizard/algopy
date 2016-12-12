4a662321fb80e7ccebb8ed8dbb75f23d57efbc41
Peter Stenger <peter.a.stenger@gmail.com>
2016-12-11 19:09:52 -0500

    save

--- ttt/alphabeta.py
+++ ttt/alphabeta.py
@@ -1,2 +1,20 @@
-#!/usr/bin/env python3
+#My mind broke. WIP
+def alpha_beta(game_state, alpha, beta):
+    if game_state.is_gameover():
+        return game_state.score()
 
+def max_play(game_state, alpha, beta):
+    if game_state.is_gameover():
+        return game_state.score()
+
+def min_play(game_state, alpha, beta):
+
+    if game_state.is_gameover():
+        return game_state.score()
+    moves = game_state.get_possible_moves()
+    for move in moves:
+        copy = game_state.get_next_state(move, False)
+        score = max_play(copy)
+
+        if beta <= alpha:
+            break
--- ttt/minimax.py
+++ ttt/minimax.py
@@ -1,3 +1,7 @@
+#http://giocc.com/concise-implementation-of-minimax-through-higher-order-functions.html
+#Lambda - https://www.programiz.com/python-programming/anonymous-function
+#Map - http://book.pythontips.com/en/latest/map_filter.html
+
 '''max and min will call on each other recusively, until a terminal state'''
 def max_play(game_state):
     '''if the game is over returns score, otherwise calls min_play on it's childen (possible moves from the state) and returns the maximum'''
@@ -12,5 +16,5 @@ def min_play(game_state):
     return min(map(lambda move: max_play(game_state.get_next_state(move, False)), game_state.get_possible_moves()))
 
 def minimax(game_state):
-    '''returns the max of mapping the (move, score) tuple to the possible move using [1] of the tuple the (score)'''
+    '''returns the max of mapping the (move, score) tuple to the possible move using the second part of the tuple (score)'''
     return max(map(lambda move: (move, min_play(game_state.get_next_state(move, True))), game_state.get_possible_moves()), key = lambda x: x[1])
