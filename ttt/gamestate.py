ef559d72662035b719e83eebfba1ba5b7fa62432
Peter Stenger <peter.a.stenger@gmail.com>
2016-12-15 16:54:06 -0500

    Update gamestate.py

--- ttt/gamestate.py
+++ ttt/gamestate.py
@@ -6,25 +6,22 @@ class GameState:
         self.winning_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
     def is_gameover(self):
-        '''returns if a game_state has been won or filled up'''
-        if self.board.count(self.char) + self.board.count(self.oppchar) == 9:
-            return True
-        for combo in self.winning_combos:
-            if self.board[combo[0]] == self.char and self.board[combo[1]] == self.char and self.board[combo[2]] == self.char:
-                return True    
-            elif self.board[combo[0]] == self.oppchar and self.board[combo[1]] == self.oppchar and self.board[combo[2]] == self.oppchar:
-                return True       
-        return False
-    def score(self):
+        '''returns if a game_state has been won or filled up''' 
+        if self.score_game() is 'NOT OVER':
+            return False
+        return True
+    def score_game(self):
         '''returns 1, 0, or -1 corresponding to the score (WIN, TIE, LOSS) from the ai's point of view'''
-        if self.board.count(self.char) + self.board.count(self.oppchar) == 9:
-            return 0
+
         for combo in self.winning_combos:
             if self.board[combo[0]] == self.char and self.board[combo[1]] == self.char and self.board[combo[2]] == self.char:
                 return 1   
             elif self.board[combo[0]] == self.oppchar and self.board[combo[1]] == self.oppchar and self.board[combo[2]] == self.oppchar:
                 return -1       
-
+        if self.board.count(self.char) + self.board.count(self.oppchar) == 9:
+            return 0
+       
+        return 'NOT OVER'
     @staticmethod
     def return_state(score):
         '''Returns a word for each score'''
@@ -35,7 +32,7 @@ class GameState:
         elif score == -1:
             return('LOST')
         else:
-            return('ERROR')
+            return('ERROR:Score was:' + str(score))
 
     def pretty_print(self):
         '''prints the board joined by spaces'''
