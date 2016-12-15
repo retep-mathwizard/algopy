afa8a43b2c5e39c5d98aefbbe13e86d37bf66aab
Peter Stenger <peter.a.stenger@gmail.com>
2016-12-15 17:01:25 -0500

    Update tutorialminimax.py

--- ttt/tutorialminimax.py
+++ ttt/tutorialminimax.py
@@ -1,21 +1,29 @@
 '''
-Tutorial at: http://giocc.com/concise-implementation-of-minimax-through-higher-order-functions.html
-Review at - http://codereview.stackexchange.com/questions/149363/minimax-tic-tac-toe-implementation/149605#149605
+http://codereview.stackexchange.com/questions/149363/minimax-tic-tac-toe-implementation/149605#149605
 '''
 from operator import itemgetter
-def max_play(game_state):
-    '''if the game is over returns score, otherwise calls min_play on it's childen (possible moves from the state) and returns the maximum'''
-    if game_state.is_gameover(): 
+
+def minimax(game_state, our_turn):
+    '''if the game is over returns (None, score), otherwise recurses to find the best move and returns it and the score.'''
+    score = game_state.get_winner()
+    if game_state.is_gameover():
         return game_state.score()
-    return max(map(lambda move: min_play(game_state.get_next_state(move, True)), game_state.get_possible_moves()))
+    moves = ((move, minimax(game_state.get_next_state(move, our_turn), not our_turn)) for move in game_state.get_possible_moves())
+    return (max if our_turn else min)(moves, key=itemgetter(1))
 
-def min_play(game_state):
-    '''if the game is over returns score, otherwise calls max_play on it's childen (possible moves from the state) and returns the minimum'''
-    if game_state.is_gameover(): 
+moves = []
+
+def expanded_minimax(game_state, our_turn):
+    global moves
+    if game_state.is_gameover():
         return game_state.score()
-    return min(map(lambda move: max_play(game_state.get_next_state(move, False)), game_state.get_possible_moves()))
-
-def minimax(game_state):
-    '''returns the max of mapping the (move, score) tuple to the possible move using [1] of the tuple the (score)'''
-    return max(map(lambda move: (move, min_play(game_state.get_next_state(move, True))), game_state.get_possible_moves()), key = itemgetter(1))
-
+    
+    for move in game_state.get_possible_moves():
+        child = game_state.get_next_state(move, our_turn)
+        score = expanded_minimax(child, not our_turn)
+        moves.append(move, score)
+        
+    if our_turn:
+        return max(moves, key=itemgetter(1))
+    else:
+        return min(moves, key=itemgetter(1))
